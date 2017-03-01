3bd91e9c2a69dbb8064e4241d8439aac2411231e
sonali28109 <sonugarud.99@gmail.com>
2017-03-01 23:50:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,14 @@
 	
 </head>
 <body >
+	<body style="background-color:powderblue;">
 	<table>
 		<tr>
 			<td style='width: 30%;'>
 				<img class = 'newappIcon' src='images/travel.gif'>
-				<style>
-					background-image:url('images/abcd.jpg');
-				</style>
+				//<style>
+					//background-image:url('images/abcd.jpg');
+				//</style>
 			</td>
 			<td>
 				<h1 id = "message"><?php echo "WatsonTourGuide"; ?></h1>
